cc0911b498ed5cb7f88be9314ce8d9e1f42f1e95
JananiVenk <86885903+JananiVenk@users.noreply.github.com>
2023-12-15 14:06:29 +0530

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,11 +1,12 @@
-import sys
+import sys,os
 from fastapi.testclient import TestClient
 sys.path.append('/home/ubuntu/janani/app')
+root_dir=os.path.dirname(__file__)
 from app.main import app
 client=TestClient(app)
 
 def test_process_image():
-    with open("/images/10006.jpg", 'rb') as image_file:
+    with open(os.path.join(root_dir,"images/10006.jpg", 'rb')) as image_file:
         files = {"file": ('10006.jpg', image_file, "image/jpg")}
         response = client.post("http://127.0.0.1:8012/predict", files=files)
         assert response.status_code == 200
