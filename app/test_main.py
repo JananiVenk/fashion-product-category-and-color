fc3c87dc77194eacaf2b8cdcf096bf5c467a6d4d
JananiVenk <86885903+JananiVenk@users.noreply.github.com>
2023-12-15 13:54:46 +0530

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -5,7 +5,8 @@ from app.main import app
 client=TestClient(app)
 
 def test_process_image():
-    with open('images/10006.jpg', 'rb') as image_file:
+    path=os.path.join(os.path.dirname(__file__,'images/10006.jpg')
+    with open(path, 'rb') as image_file:
         files = {"file": ('10006.jpg', image_file, "image/jpg")}
         response = client.post("http://127.0.0.1:8012/predict", files=files)
         assert response.status_code == 200
